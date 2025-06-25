7a4e149c66598a533747029cc1b43da277e68b59
ThaaraniShivakumar <sivathaarani2004@gmail.com>
2025-06-25 09:35:48 +0530

    Tried to fetch the data from api and then displayed

--- ReactTask/react_task/src/AccountSummary.jsx
+++ ReactTask/react_task/src/AccountSummary.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react"
+
 export const AccountSummary = () =>{
-    return{
-        
-    }
+const[data,setData]=useState([]) 
+
+    useEffect(()=>{
+      
+         fetch("https://mocki.io/v1/3bd581b2-0757-4de7-82b2-0bd333fc8816")
+         .then(res=> res.json())
+         .then(data =>setData(data))
+
+    },[])
+
+
+    return(
+        <div>
+            {
+              data.map((getData)=>(
+                <div key={getData.id}>
+                    <h4>{getData.date}</h4>
+                </div>
+              ))
+            }
+        </div>
+    )
 }
--- ReactTask/react_task/src/App.js
+++ ReactTask/react_task/src/App.js
@@ -1,9 +1,10 @@
+import { AccountSummary } from './AccountSummary';
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+      <AccountSummary/>
     </div>
   );
 }
